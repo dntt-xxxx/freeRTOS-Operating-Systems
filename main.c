9f6f9bf244baf26aba621ccd461c03a3d73e16cc
Dnttxx <dongoctuc@gmail.com>
2022-12-17 23:12:17 +0700

    update newline in main.c file

--- main.c
+++ main.c
@@ -20,7 +20,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
-
+//this line adding while using branch MAIN
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
